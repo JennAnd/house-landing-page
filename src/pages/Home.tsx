257df583dc23f3ff578c59f5477cee26b4aa4332
Jennifer Andersson <jennifer_andersson90@hotmail.com>
2025-12-21 21:43:28 +0100

    add text section content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import TextSection from "../components/TextSection";
 
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <TextSection />
     </>
   );
 }
