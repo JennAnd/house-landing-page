11eb07fa4b3635a3d1e069223405c8695709ec89
Jennifer Andersson <jennifer_andersson90@hotmail.com>
2025-12-21 20:40:54 +0100

    add header navigation icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
+import menuIcon from "../assets/menu.svg";
+
 function Header() {
-  return <header>House</header>;
+  return (
+    <header>
+      <div>House</div>
+      <nav>
+        <button aria-label="Menu">
+          <img src={menuIcon} alt="" />
+        </button>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
