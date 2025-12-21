30f22679019e02ec83fd8f1737594d189f6addf9
Jennifer Andersson <jennifer_andersson90@hotmail.com>
2025-12-21 20:54:59 +0100

    add hero content structure

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Unika hus i en unik miljö</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac erat
+        tempor, fringilla nunc vitae, laoreet velit. Suspendisse eu tortor eu
+        odio dapibus molestie nec quis ipsum.
+      </p>
+      <div>
+        <button>Läs mer</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
